5a3c93b1c1192673261c2d9826f54aa7f8157dac
EduardoConteCesul <eduardo.2024014337@alunocesul.com>
2025-08-21 22:29:07 -0300

    Configurado maven e fxml

---
+++ src/main/java/com/contedev/expensetracker/MainApp.java
@@ -0,0 +1,26 @@
+package com.contedev.expensetracker;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainApp extends Application
+{
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("src/main/resources/com/contedev/expensetracker/ExpenseView.fxml"));
+
+        Parent root = loader.load();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Expense Tracker - MVVM");
+        stage.show();
+    }
+
+    public static void main(String[] args ){launch();}
+}
